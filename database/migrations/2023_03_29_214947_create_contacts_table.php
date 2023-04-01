86afaed3cede9d6f78eb6316904a8273ff7bdb15
Edber Vuciterna <edberv1@gmail.com>
2023-04-01 18:15:28 +0200

    Migrations

--- database/migrations/2023_03_29_214947_create_contacts_table.php
+++ database/migrations/2023_03_29_214947_create_contacts_table.php
@@ -1,32 +1,36 @@
 <?php
-
+  
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+  
 return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('roles')->default('user');
-            $table->string('password');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('subject');
+            $table->text('message');
             $table->timestamps();
-            
         });
     }
-
+  
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 };
