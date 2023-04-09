99872553d34d3d93e7234631c683b62657e41110
Edber Vuciterna <edberv1@gmail.com>
2023-04-09 16:59:14 +0200

    layout, messages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,193 @@
+<x-layout>
+
+
+    
+    <section>
+        <div class="grid grid-cols-5 gap-10 px-10 pt-10 pb-5 items-center md:items-start md:px-20 md:pt-20">
+            <div class="col-span-3">
+                <div class="pb-8 md:pb-16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="69" height="100" viewBox="0 0 69 50">
+                        <text id="_" data-name="&quot;" transform="translate(0 75)" fill="#F62006" font-size="120" font-family="Helvetica"><tspan x="0" y="0">&quot;</tspan></text>
+                      </svg>
+                </div>
+                <div>
+                    <h1 class="text-xl text-gray-500 md:text-5xl pb-4 md:pb-8 font-bold">OUR TEAM</h1>
+                    <p class="text-laravel pr-12 text-sm md:text-xl">This is the team that takes care of our clients!</p>
+                </div>
+            </div>
+
+
+            <div class="col-span-2 ">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel z-1 py-2 px-3 md:px-16 md:py-9 flex flex-col text-white">
+                    <span class="text-xs md:text-3xl font-bold">
+                        Ervin Shehu
+                    </span>
+                    <span class="text-[8px] md:text-xl">
+                        CEO
+                    </span>
+                </div>
+            </div>
+        </div>
+        
+
+        <div class="grid grid-cols-3 mb-14 md:grid-cols-6 gap-4 px-10 md:pt-16 md:pb-28 md:px-20">
+            <div class="md:flex-col">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel py-2 px-3 md:py-4 md:px-6 flex flex-col text-white">
+                    <span class="text-s font-bold">
+                        Edber Vuçitërna
+                    </span>
+                    <span class="text-[12px]">
+                        CTO
+                    </span>
+                </div>
+            </div>
+
+            <div class="flex-col">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel py-2 px-3 md:py-4 md:px-6 flex flex-col text-white">
+
+                    <span class="text-s font-bold">
+                        Adonis Çmega
+                    </span>
+                    <span class="text-[12px]">
+                        CFO
+                    </span>
+                </div>
+            </div>
+            <div class="flex-col">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel py-2 px-3 md:py-4 md:px-6 flex flex-col text-white">
+                    <span class="text-s font-bold">
+                        Haris Çmega
+                    </span>
+                    <span class="text-[12px]">
+                        Executive Team
+                    </span>
+                </div>
+            </div>
+            <div class="flex-col">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel py-2 px-3 md:py-4 md:px-6 flex flex-col text-white">
+                    <span class="text-s font-bold">
+                        Esned Spahiu
+                    </span>
+                    <span class="text-[12px]">
+                        Executive Team
+                    </span>
+                </div>
+            </div>
+            <div class="flex-col">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel py-2 px-3 md:py-4 md:px-6 flex flex-col text-white">
+                    <span class="text-s font-bold">
+                        Besim Çmega
+                    </span>
+                    <span class="text-[12px]">
+                        Executive Team
+                    </span>
+                </div>
+            </div>
+            <div class="flex-col">
+                <div>
+                    <img src="https://images.unsplash.com/photo-1623366302587-b38b1ddaefd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
+                        alt="">
+                </div>
+                <div class="bg-laravel py-2 px-3 md:py-4 md:px-6 flex flex-col text-white">
+                    <span class="text-s font-bold">
+                        Selman Vuçitërna
+                    </span>
+                    <span class="text-[12px]">
+                        Executive Team
+                    </span>
+                </div>
+            </div>
+               
+        </div>
+    </section>
+
+    <div class="container my-24 px-6 mx-auto">
+  
+        <!-- Section: Design Block -->
+        <section class="mb-32 text-center">
+          <h2 class="text-3xl text-gray-500 font-bold mb-12">
+            There are good reasons to <span class="text-laravel">be proud</span>
+          </h2>
+          <div class="grid lg:gap-x-12 md:grid-cols-3">
+            <div class="mb-12 md:mb-0">
+              <div class="p-4 bg-laravel rounded-md shadow-lg inline-block mb-6">
+                <svg
+                  class="w-5 h-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 496 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm80 144c23.8 0 52.7 29.3 56 71.4.7 8.6-10.8 11.9-14.9 4.5l-9.5-17c-7.7-13.7-19.2-21.6-31.5-21.6s-23.8 7.9-31.5 21.6l-9.5 17c-4.1 7.3-15.6 4-14.9-4.5 3.1-42.1 32-71.4 55.8-71.4zm-160 0c23.8 0 52.7 29.3 56 71.4.7 8.6-10.8 11.9-14.9 4.5l-9.5-17c-7.7-13.7-19.2-21.6-31.5-21.6s-23.8 7.9-31.5 21.6l-9.5 17c-4.2 7.4-15.6 4-14.9-4.5 3.1-42.1 32-71.4 55.8-71.4zm80 280c-60.6 0-134.5-38.3-143.8-93.3-2-11.9 9.4-21.6 20.7-17.9C155.1 330.5 200 336 248 336s92.9-5.5 123.1-15.2c11.4-3.7 22.6 6.1 20.7 17.9-9.3 55-83.2 93.3-143.8 93.3z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold text-laravel mb-4">1000+</h3>
+              <h5 class="text-lg font-medium text-gray-500">HAPPY COSTUMERS</h5>
+            </div>
+      
+            <div class="mb-12 md:mb-0">
+              <div class="p-4 bg-laravel rounded-md shadow-lg inline-block mb-6">
+                <svg
+                  class="w-5 h-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z"
+                  ></path>
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold text-laravel mb-4">70%</h3>
+              <h5 class="text-lg font-medium text-gray-500">GROWTH</h5>
+            </div>
+      
+            <div class="mb-12 md:mb-0">
+              <div class="p-4 bg-laravel rounded-md shadow-lg inline-block mb-6">
+                <svg
+                  class="w-5 h-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 640 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M512.1 191l-8.2 14.3c-3 5.3-9.4 7.5-15.1 5.4-11.8-4.4-22.6-10.7-32.1-18.6-4.6-3.8-5.8-10.5-2.8-15.7l8.2-14.3c-6.9-8-12.3-17.3-15.9-27.4h-16.5c-6 0-11.2-4.3-12.2-10.3-2-12-2.1-24.6 0-37.1 1-6 6.2-10.4 12.2-10.4h16.5c3.6-10.1 9-19.4 15.9-27.4l-8.2-14.3c-3-5.2-1.9-11.9 2.8-15.7 9.5-7.9 20.4-14.2 32.1-18.6 5.7-2.1 12.1.1 15.1 5.4l8.2 14.3c10.5-1.9 21.2-1.9 31.7 0L552 6.3c3-5.3 9.4-7.5 15.1-5.4 11.8 4.4 22.6 10.7 32.1 18.6 4.6 3.8 5.8 10.5 2.8 15.7l-8.2 14.3c6.9 8 12.3 17.3 15.9 27.4h16.5c6 0 11.2 4.3 12.2 10.3 2 12 2.1 24.6 0 37.1-1 6-6.2 10.4-12.2 10.4h-16.5c-3.6 10.1-9 19.4-15.9 27.4l8.2 14.3c3 5.2 1.9 11.9-2.8 15.7-9.5 7.9-20.4 14.2-32.1 18.6-5.7 2.1-12.1-.1-15.1-5.4l-8.2-14.3c-10.4 1.9-21.2 1.9-31.7 0zm-10.5-58.8c38.5 29.6 82.4-14.3 52.8-52.8-38.5-29.7-82.4 14.3-52.8 52.8zM386.3 286.1l33.7 16.8c10.1 5.8 14.5 18.1 10.5 29.1-8.9 24.2-26.4 46.4-42.6 65.8-7.4 8.9-20.2 11.1-30.3 5.3l-29.1-16.8c-16 13.7-34.6 24.6-54.9 31.7v33.6c0 11.6-8.3 21.6-19.7 23.6-24.6 4.2-50.4 4.4-75.9 0-11.5-2-20-11.9-20-23.6V418c-20.3-7.2-38.9-18-54.9-31.7L74 403c-10 5.8-22.9 3.6-30.3-5.3-16.2-19.4-33.3-41.6-42.2-65.7-4-10.9.4-23.2 10.5-29.1l33.3-16.8c-3.9-20.9-3.9-42.4 0-63.4L12 205.8c-10.1-5.8-14.6-18.1-10.5-29 8.9-24.2 26-46.4 42.2-65.8 7.4-8.9 20.2-11.1 30.3-5.3l29.1 16.8c16-13.7 34.6-24.6 54.9-31.7V57.1c0-11.5 8.2-21.5 19.6-23.5 24.6-4.2 50.5-4.4 76-.1 11.5 2 20 11.9 20 23.6v33.6c20.3 7.2 38.9 18 54.9 31.7l29.1-16.8c10-5.8 22.9-3.6 30.3 5.3 16.2 19.4 33.2 41.6 42.1 65.8 4 10.9.1 23.2-10 29.1l-33.7 16.8c3.9 21 3.9 42.5 0 63.5zm-117.6 21.1c59.2-77-28.7-164.9-105.7-105.7-59.2 77 28.7 164.9 105.7 105.7zm243.4 182.7l-8.2 14.3c-3 5.3-9.4 7.5-15.1 5.4-11.8-4.4-22.6-10.7-32.1-18.6-4.6-3.8-5.8-10.5-2.8-15.7l8.2-14.3c-6.9-8-12.3-17.3-15.9-27.4h-16.5c-6 0-11.2-4.3-12.2-10.3-2-12-2.1-24.6 0-37.1 1-6 6.2-10.4 12.2-10.4h16.5c3.6-10.1 9-19.4 15.9-27.4l-8.2-14.3c-3-5.2-1.9-11.9 2.8-15.7 9.5-7.9 20.4-14.2 32.1-18.6 5.7-2.1 12.1.1 15.1 5.4l8.2 14.3c10.5-1.9 21.2-1.9 31.7 0l8.2-14.3c3-5.3 9.4-7.5 15.1-5.4 11.8 4.4 22.6 10.7 32.1 18.6 4.6 3.8 5.8 10.5 2.8 15.7l-8.2 14.3c6.9 8 12.3 17.3 15.9 27.4h16.5c6 0 11.2 4.3 12.2 10.3 2 12 2.1 24.6 0 37.1-1 6-6.2 10.4-12.2 10.4h-16.5c-3.6 10.1-9 19.4-15.9 27.4l8.2 14.3c3 5.2 1.9 11.9-2.8 15.7-9.5 7.9-20.4 14.2-32.1 18.6-5.7 2.1-12.1-.1-15.1-5.4l-8.2-14.3c-10.4 1.9-21.2 1.9-31.7 0zM501.6 431c38.5 29.6 82.4-14.3 52.8-52.8-38.5-29.6-82.4 14.3-52.8 52.8z"
+                  />
+                </svg>
+              </div>
+              <h3 class="text-2xl font-bold text-laravel mb-4">50+</h3>
+              <h5 class="text-lg font-medium text-gray-500">TEAM SIZE</h5>
+            </div>
+          </div>
+        </section>
+        <!-- Section: Design Block -->
+        
+      </div>
+      
+</x-layout>
+
