872e523607a65b6c4e6a233f1144020837ac4b86
Edber Vuciterna <edberv1@gmail.com>
2023-05-13 17:03:39 +0200

    Dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+class DashboardController extends Controller
+{
+    public function dashboard()
+    {
+
+        return view('dashboard');
+
+    }
+
+}
