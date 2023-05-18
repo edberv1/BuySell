72c7361a985ca3c77cac91068d33a20aabd97818
edberv1 <edberv1@gmail.com>
2023-05-18 18:20:13 +0200

    messages-administrations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -288,3 +288,73 @@
         </tbody>
     </table>
 </div>
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <header class="card-header" id="contacts">
+            <p class="card-header-title">
+              <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
+              Messages
+            </p>
+          </header>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Email
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Phone
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Subject
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Message
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Recieved at
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($contacts as $item)
+
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $item->id }}
+                </th>
+                <td class="px-6 py-4">
+                {{ $item->name }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->email }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->phone }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->subject }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->message }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->created_at }}
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
