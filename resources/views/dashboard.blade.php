11c0631038dfa7c6792c1031a1db982388941c77
edberv1 <edberv1@gmail.com>
2023-05-18 18:19:22 +0200

    users-administration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -146,3 +146,68 @@
     </div>
 
     </div>
+
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <header class="card-header" id="users">
+            <p class="card-header-title">
+              <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
+              Users
+            </p>
+          </header>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Email
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Role
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Password
+                </th>
+                <th scope="col" class="px-6 py-3">
+                Created
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($userat as $item)
+
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $item->id }}
+                </th>
+                <td class="px-6 py-4">
+                {{ $item->name }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->email }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->roles }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->password }}
+                </td>
+                <td class="px-6 py-4">
+                {{ $item->created_at }}
+                </td>
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
