3e8fce787d7f36d3c0ae77ae0e7828e2286db899
edberv1 <edberv1@gmail.com>
2023-05-19 23:14:05 +0200

    contactForm-Success

--- resources/views/contactFrom.blade.php
+++ resources/views/contactFrom.blade.php
@@ -8,3 +8,16 @@
         <source src="{{ asset('images/contact.mp4') }}" type="video/mp4">
       </video>
     </div>
+
+    
+    <div class="container text-gray-800 px-4 md:px-12">
+      <div class="block rounded-lg shadow-lg py-10 md:py-12 px-2 md:px-6"
+        style="margin-top: -100px; background: hsla(0, 0%, 100%, 0.8); backdrop-filter: blur(30px);">
+        <div class="flex flex-wrap">
+          <div class="grow-0 shrink-0 basis-auto w-full xl:w-5/12 px-3 lg:px-6 mb-12 xl:mb-0">
+            
+            @if(Session::has('success'))
+            <div class="text-green-500 text-xs mt-1">
+                {{Session::get('success')}}
+            </div>
+            @endif
