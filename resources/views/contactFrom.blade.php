f46a1221c973d8533a44620674711e25929eb3d6
edberv1 <edberv1@gmail.com>
2023-05-19 23:18:01 +0200

    ContactDesignImprove

--- resources/views/contactFrom.blade.php
+++ resources/views/contactFrom.blade.php
@@ -85,3 +85,91 @@ Submit
 </button>
 </form>
 </div>
+
+<h1 class="text-6xl text-center">LET'S GET IN TOUCH</h1>
+            <p class="text-xl pb-4 text-center">We'll get back to you in 24h.</p>
+            <div class="flex flex-wrap">
+                
+              <div class="mb-12 grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                
+                <div class="flex items-start">
+                    
+                  <div class="shrink-0">
+                    <div class="p-4 bg-laravel rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                      <i class="fa-solid w-3 text-white fa-location-dot"></i>
+                    </div>
+                  </div>
+                  
+                  <div class="grow ml-6">
+                    <p class="font-bold mb-1">ADDRESS</p>
+                    <p class="text-gray-500">Pejton Prishtine</p>
+                    
+                   
+                  </div>
+                </div>
+              </div>
+              <div class="mb-12 grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                <div class="flex items-start">
+                  <div class="shrink-0">
+                    <div class="p-4 bg-laravel rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                      <i class="fa-solid w-3 text-white fa-phone"></i>
+                    </div>
+                  </div>
+                  <div class="grow ml-6">
+                    <p class="font-bold mb-1">PHONE</p>
+                    <p class="text-gray-500">+383 44 123 456</p>
+                  </div>
+                </div>
+              </div>
+              <div class="mb-12 md:mb-0 grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                <div class="flex align-start">
+                  <div class="shrink-0">
+                    <div class="p-4 bg-laravel rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                      <i class="fa-solid w-3 text-white fa-envelope"></i>
+                    </div>
+                  </div>
+                  <div class="grow ml-6">
+                    <p class="font-bold mb-1">EMAIL</p>
+                    <p class="text-gray-500">info@buysell.com</p>
+                  </div>
+                </div>
+              </div>
+              <div class="grow-0 shrink-0 basis-auto w-full md:w-6/12 px-3 lg:px-6">
+                <div class="flex align-start">
+                  <div class="shrink-0">
+                    <div class="p-4 bg-laravel rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bug" class="w-5 text-white"
+                        role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                          d="M511.988 288.9c-.478 17.43-15.217 31.1-32.653 31.1H424v16c0 21.864-4.882 42.584-13.6 61.145l60.228 60.228c12.496 12.497 12.496 32.758 0 45.255-12.498 12.497-32.759 12.496-45.256 0l-54.736-54.736C345.886 467.965 314.351 480 280 480V236c0-6.627-5.373-12-12-12h-24c-6.627 0-12 5.373-12 12v244c-34.351 0-65.886-12.035-90.636-32.108l-54.736 54.736c-12.498 12.497-32.759 12.496-45.256 0-12.496-12.497-12.496-32.758 0-45.255l60.228-60.228C92.882 378.584 88 357.864 88 336v-16H32.666C15.23 320 .491 306.33.013 288.9-.484 270.816 14.028 256 32 256h56v-58.745l-46.628-46.628c-12.496-12.497-12.496-32.758 0-45.255 12.498-12.497 32.758-12.497 45.256 0L141.255 160h229.489l54.627-54.627c12.498-12.497 32.758-12.497 45.256 0 12.496 12.497 12.496 32.758 0 45.255L424 197.255V256h56c17.972 0 32.484 14.816 31.988 32.9zM257 0c-61.856 0-112 50.144-112 112h224C369 50.144 318.856 0 257 0z">
+                        </path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="grow ml-6">
+                    <p class="font-bold mb-1">Bug report</p>
+                    <p class="text-gray-500">bugs@buysell.com</p>
+  
+                  </div>
+                </div>
+                
+                
+
+              </div>
+              
+            </div>
+            <div class="mt-20 md:mb-12 lg:mb-0">
+                <div class="map-container relative shadow-lg rounded-lg">
+                    <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Pejton%20Prishtine+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+
+
+</div>
