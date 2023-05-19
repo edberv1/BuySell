4ee02719f0190a8692ceda8aeba78ebc8cac64a2
Ervin Shehu <96729631+ervinshehu@users.noreply.github.com>
2023-05-19 23:03:10 +0200

    DashboardStatistics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,13 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
+use App\Models\User;
+use App\Models\Listing;
+use App\Models\Contact;
+use Illuminate\Support\Facades\Redirect;
+
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-
-        return view('dashboard');
-
+        $users = User::where('roles', 'user')->count();
+        $admins = User::where('roles', 'admin')->count();
+        $contact = Contact::count();
+        $contacts = Contact::all();
+        $listing = Listing::count();
+        $posts = Listing::all();
+        $userat = User::all();
+        return view('dashboard', compact('userat','users', 'admins', 'listing', 'contact', 'posts', 'contacts'));
     }
 
 }
