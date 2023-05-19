ea64bf9e6c0a1c7a69d1f5cfc8281ca56c30db2f
edberv1 <edberv1@gmail.com>
2023-05-19 23:08:25 +0200

    home-hero

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+
+
+        <section class="mb-10">
+            <div class="overflow-hidden object-content" style="height: 500px; ">
+                <video autoplay muted loop id="vid">
+                    <source src="{{ asset('images/video.mp4') }}" type="video/mp4">
+                        
+                </video>
+            </div>
+            
+            <div class="container mx-auto px-6 md:px-12 xl:px-32">
+              <div class="text-center text-gray-800">
+                <div class="block rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12" style="margin-top: -170px; background: hsla(0, 0%, 100%, 0.7); backdrop-filter: blur(30px);">
+                  <h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The best offers on the market <br /><span class="text-laravel">for you.</span></h1>
+                  <a class="inline-block px-7 py-3 mb-2 md:mb-0 mr-0 md:mr-2 bg-laravel text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-black hover:shadow-lg focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out animate-bounce" data-mdb-ripple="true" data-mdb-ripple-color="light" href="/register" role="button">Get started</a>
+                  <a class="inline-block px-7 py-3 mb-2 md:mb-0 mr-0 md:mr-2 bg-laravel text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-black hover:shadow-lg focus:bg-blackfocus:shadow-lg focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#contact" role="button">Conact Us</a>
+                </div>
+              </div>
+            </div>
+          </section>
