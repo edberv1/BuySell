fb4e7ac5f31851aba3fe8e232d0b9d1672808e65
Ervin Shehu <96729631+ervinshehu@users.noreply.github.com>
2023-05-19 23:13:36 +0200

    ContactFormHeader

---
+++ resources/views/contactFrom.blade.php
@@ -0,0 +1,10 @@
+<div class="container my-24 px-6 mx-auto" id="contact">
+<section class="mb-32 text-gray-800">
+
+    <div class="relative overflow-hidden bg-no-repeat bg-cover"
+      style="background-position: 50%; height: 300px;">
+
+      <video autoplay muted loop >
+        <source src="{{ asset('images/contact.mp4') }}" type="video/mp4">
+      </video>
+    </div>
