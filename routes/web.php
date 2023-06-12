da8760937ade5544e2844df76df7263a6a565932
edberv1 <edberv1@gmail.com>
2023-06-12 15:41:47 +0200

    Routes-reorganised

--- routes/web.php
+++ routes/web.php
@@ -16,40 +16,48 @@ use Illuminate\Support\Facades\Route;
 // Dashboard
 Route::get('/dashboard', [DashboardController::class, 'dashboard'])->middleware('admin');
 
-// All Listings
-Route::get('/', [ListingController::class, 'index']);
-
-//Show Create Form
-Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
-
-Route::post('/listings', [ListingController::class, 'storing'])->name('listings.storing');
-
-//Store Listing
-// Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
-//Update Listing
-// Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
-
-//Show Edit Form
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
-
-
-Route::get('/users/{user}/edit', 'App\Http\Controllers\UserController@edit')->name('users.edit');
-Route::put('/users/{user}', 'App\Http\Controllers\UserController@update')->name('users.update');
-
+//Listing Edit
 Route::get('/listings/{listing}/edition', 'App\Http\Controllers\ListingController@edition')->name('listings.edition');
 Route::put('/listings/{listing}', 'App\Http\Controllers\ListingController@updation')->name('listings.updation');
-
-
-
-
-//Delete Lisitng
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
 //Admin Delete Listing
 Route::delete('/listings/{listing}', 'App\Http\Controllers\DashboardController@destroy')->name('listings.destroy');
 
 //Admin Delete User
 Route::delete('/users/{user}', 'App\Http\Controllers\DashboardController@userDestroy')->name('users.destroy');
+
+//Delete Contact
+Route::delete('/contact-us/{id}', 'App\Http\Controllers\DashboardController@delete')->name('deleteContact');
+
+//Create a new user AdminDashboard
+Route::post('/users', 'App\Http\Controllers\UserController@creating')->name('users.creating');
+Route::get('/users', 'App\Http\Controllers\UserController@index')->name('users.index');
+
+//New Listing
+Route::post('/listings', [ListingController::class, 'storing'])->name('listings.storing');
+
+//Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+
+
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+//Delete Lisitng
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
+
+//Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+//User Edit
+Route::get('/users/{user}/edit', 'App\Http\Controllers\UserController@edit')->name('users.edit');
+Route::put('/users/{user}', 'App\Http\Controllers\UserController@update')->name('users.update');
+
+//Store Listing
+// Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+//Update Listing
+// Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
 
 //Manage Listings
 Route::get('listings/manage', [ListingController::class, 'manage'])->middleware('auth');
@@ -61,26 +69,17 @@ Route::get('/listings/{listing}', [ListingController::class, 'show']);
 Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 //Show About
-Route::get('/about', function(){
-    return view('about');
-});
+Route::get('/about', function(){ return view('about');});
 
 //Show Contact
 Route::get('contact-us', [ContactController::class, 'index']);
 Route::post('contact-us', [ContactController::class, 'store'])->name('contact.us.store');
-
-Route::delete('/contact-us/{id}', 'App\Http\Controllers\DashboardController@delete')->name('deleteContact');
 
 //Show Marketplace
 Route::get('/home', [ListingController::class, 'home'])->name('home');
 
 //Create New User
 Route::post('/user', [UserController::class, 'store']);
-
-//Create a new user AdminDashboard
-Route::post('/users', 'App\Http\Controllers\UserController@creating')->name('users.creating');
-Route::get('/users', 'App\Http\Controllers\UserController@index')->name('users.index');
-
 
 //Log User Out
 Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
