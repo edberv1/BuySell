1095e07e004cc470cbbf124594de6f0219bfbb7e
edberv1 <edberv1@gmail.com>
2023-06-12 15:49:24 +0200

    DashboardController-edit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
-
 use App\Models\User;
 use App\Models\Listing;
 use App\Models\Contact;
@@ -48,5 +47,10 @@ class DashboardController extends Controller
 
     return redirect()->back()->with('success', 'Contact deleted successfully.');
 }
+
+
+
+
+
 
 }
